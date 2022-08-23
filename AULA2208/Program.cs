1ad399f948b8743bd3f8f9581f1e923594a41e5a
curso.ads <curso.ads@LAB06-16>
2022-08-22 22:12:20 -0300

    Adicionar arquivos de projeto.

---
+++ AULA2208/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AULA2208
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // declaração e instanciação do objeto
+            Produto prod = new Produto();
+            Cliente cli = new Cliente();
+
+            Console.Write("Digite o nome do Cliente: ");
+            cli.setNome(Console.ReadLine());
+            Console.Write("Digite o CPF do cliente: ");
+            cli.setCpf(Console.ReadLine());
+            Console.Write("Digite o Endereço do cliente: ");
+            cli.setEndereco(Console.ReadLine());
+            Console.Write("Digite a idade do cliente: ");
+            cli.setIdade(Convert.ToInt32(Console.ReadLine()));
+            Console.Write("Digite o código do produto: ");
+            prod.setCodigo(Convert.ToInt32(Console.ReadLine()));
+            Console.Write("Digite a descrição: ");
+            prod.setDescricao(Console.ReadLine());
+            Console.Write("Digite a categoria: ");
+            prod.setCategoria(Console.ReadLine());
+            Console.Write("Digite o valor: ");
+            prod.setValor(Convert.ToDouble(Console.ReadLine()));
+
+            Console.WriteLine("Nome = " + cli.getNome() + "\nEndereco = " +
+                cli.getEndereco() + "\nCPF = " + cli.getCpf() + "\nIdade = "
+                + cli.getIdade());
+
+            Console.WriteLine("Código = " + prod.getCodigo() + "\nDescrição: " +
+               prod.getDescricao() + "\nCategoria = " + prod.getCategoria() +
+               "\nValor = " + prod.getValor().ToString("C"));
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
